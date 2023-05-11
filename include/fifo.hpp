50a5e6a8a7356c5e5581238fae90afd392fe1c3d
RosasGabriel <gabriel.rosas7232@gmail.com>
2023-05-11 14:02:51 -0300

    First structuring of the FIFO paging algorithm

---
+++ include/fifo.hpp
@@ -0,0 +1,23 @@
+#ifndef FIFO_H
+#define FIFO_H
+
+#include <string>
+#include <vector>
+
+class Fifo
+{
+public:
+    Fifo();
+    
+    void readInputFile(const std::string& file_name);
+    void execute();
+    void displayResults();
+    
+private:
+    std::vector<int> page_requests;
+
+    int num_frames;
+};
+
+
+#endif // FIFO_H
---
+++ src/fifo.cpp
@@ -0,0 +1,21 @@
+#include "../include/fifo.hpp"
+
+Fifo::Fifo()
+{
+    num_frames = 0;
+}
+
+void Fifo::readInputFile(const std::string& file_name)
+{
+
+}
+
+void Fifo::execute()
+{
+
+}
+
+void Fifo::displayResults()
+{
+
+}
